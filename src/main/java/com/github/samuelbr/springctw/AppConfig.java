c313039aaddcb6d604a16633ee9d242e2fa14b02
samuelbr <samuel.brezani@gmail.com>
2014-03-16 21:14:34 +0100

    Update pom.xml

--- src/main/java/com/github/samuelbr/springctw/AppConfig.java
+++ src/main/java/com/github/samuelbr/springctw/AppConfig.java
@@ -2,9 +2,11 @@ package com.github.samuelbr.springctw;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages="com.github.samuelbr.springctw")
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class AppConfig {
 
 }
